3ed562c1ca1e765fc0a9e7719cb5262698a02db1
IanDellosa <22ian.dellosa@gmail.com>
2025-10-06 00:25:07 -0700

    Camera Controls

---
+++ Assets/PlayerCam.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerCam : NetworkBehaviour
+{
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    [SerializeField] private Transform orientation;
+
+    [SerializeField] private float xRot;
+    [SerializeField] private float yRot;
+
+    private void Start()
+    {
+        if(!IsOwner) return;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if(!IsOwner) return;
+
+        //mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRot += mouseX;
+        xRot -= mouseY;
+
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+        // orientation
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        orientation.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+}
